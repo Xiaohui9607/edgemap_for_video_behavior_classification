1d1a1d9b7c569ac9de9383a7776e5eddc1b5d97a
Ramtin Hosseini <ramtin.hosseini@tufts.edu>
2020-06-21 18:55:47 -0400

    update lbp

--- pyimagesearch/localbinarypatterns.py
+++ pyimagesearch/localbinarypatterns.py
@@ -3,11 +3,24 @@ import numpy as np
 from PDBF import rgbpdbfs
 import cv2
 
+
+def crop(im):
+    height, width = im.shape[1:]
+    width = max(height, width)
+    im = im[:, :width, :width]
+    return im
+
 class LocalBinaryPatterns:
     def __init__(self, numPoints, radius):
         # store the number of points and radius
         self.numPoints = numPoints
         self.radius = radius
+
+    def crop_image(self, image):
+        height, width = image.shape[:2]
+        width = max(height, width)
+        image = image[:width, :width, :]
+        return image
 
     def describe(self, image, eps=1e-7, flag='normal'): #flag: either normal or PDBF
         # compute the Local Binary Pattern representation
@@ -15,10 +28,13 @@ class LocalBinaryPatterns:
         # to build the histogram of patterns
 
         image=io.imread(image)
+        image = self.crop_image(image)
+
         if flag=='PDBF':
             image = rgbpdbfs(image, nbitplanes=[3], decomp_method=0, p_code=-1, n_code=-1) * 255
 
         image = color.rgb2gray(image)
+
         lbp = feature.local_binary_pattern(image, self.numPoints,
                                            self.radius, method="uniform")
         (hist, _) = np.histogram(lbp.ravel(),
